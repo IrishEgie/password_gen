99f5bfb08cff5bd647d3b55bb14d4044ed856a2b
ejarao <irishegie.arao@ssu.edu.ph>
2024-08-20 14:28:15 +0800

    stuck on creating a solution that will allow me to insert random ass letters on random index until the desired length of password is met

--- password_gen_random.py
+++ password_gen_random.py
@@ -4,11 +4,18 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+#nr_symbols = int(input(f"How many symbols would you like?\n"))
+#nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 import random as rd
 
-rd_pass = []
+pass_len = nr_letters#+nr_symbols+nr_numbers
+
+rd_pass = []#final product
+
+for rep in range(0,pass_len+1):
+    rd_l = rd.randint(0,len(letters)-1)
+    rd_pass.insert(letters[rd_l])
 
 
+print(rd_pass)
