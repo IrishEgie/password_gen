ace3d989c501f0a98b22479387059ad3a913a685
ejarao <irishegie.arao@ssu.edu.ph>
2024-08-20 15:53:56 +0800

    donnneeeee!!!!!!

--- password_gen_random.py
+++ password_gen_random.py
@@ -4,16 +4,26 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
-#nr_symbols = int(input(f"How many symbols would you like?\n"))
-#nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 import random as rd
 
-pass_len = nr_letters#+nr_symbols+nr_numbers
+pass_len = nr_letters+nr_symbols+nr_numbers
 
-for rep in range(0,pass_len+1):
+rd_pass = []
+
+for rep in range(nr_letters):
     rd_letter = rd.choice(letters)
-    
+    rd_pass.append(rd_letter) 
+for rep in range(nr_numbers):
+    rd_num = rd.choice(numbers)
+    rd_pass.append(rd_num)
+for rep in range(nr_symbols): 
+    rd_sym = rd.choice(symbols)
+    rd_pass.append(rd_sym) 
 
+rd.shuffle(rd_pass)
 
-print()
+password = ''.join(rd_pass)
+print(f"Your password is: {password}")
