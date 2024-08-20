095a5b751e11681b02ca8b8f80bbfbf4911aad58
ejarao <irishegie.arao@ssu.edu.ph>
2024-08-20 12:25:59 +0800

    Created the sequential password generator

--- password_gen_sequential.py
+++ password_gen_sequential.py
@@ -4,15 +4,22 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
-#nr_symbols = int(input(f"How many symbols would you like?\n"))
-#nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 import random as rd
 
-pass_len = nr_letters
-
+pass_len = nr_letters+nr_symbols
+print("Your password is: ")
 for rep in range(1,nr_letters+1):
-    rd_l_num = rd.randint(0,len(letters))
+    rd_l_num = rd.randint(0,len(letters)-1)
     pass_letters = letters[rd_l_num]
-    print(f"{pass_letters}")
-
+    print(pass_letters, end='')
+for rep in range(1,nr_symbols+1):
+    rd_s_num = rd.randint(0,len(symbols)-1)
+    pass_symbols = symbols[rd_s_num]
+    print(pass_symbols, end='')
+for rep in range(1,nr_numbers+1):
+    rd_n_num = rd.randint(0,len(symbols)-1)
+    pass_num = numbers[rd_n_num]
+    print(pass_num, end='')
